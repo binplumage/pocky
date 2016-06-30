75a816bb040670858759314f2e03534391998ea7
Ruru Chen <balaja2179@gmail.com>
2016-06-30 20:29:19 +0800

    1. Initial commit. 2. Create gui that can choose file and execute.
     Changes to be committed:
    	new file:   gui.py

---
+++ gui.py
@@ -0,0 +1,41 @@
+#-*- coding: utf-8 -*-
+import Tkinter as tk
+import ttk
+import tkMessageBox
+import tkFileDialog
+import sys
+import setup_env
+
+
+class MainApplication():
+    def __init__(self, master):
+        self.master = master
+        self.frame = tk.Frame(self.master)
+        self.run_button = tk.Button(self.frame, text = u"執行", width = 10, command = self.run)
+        self.chose_button = tk.Button(self.frame, text = u"選擇檔案", width = 10, command = self.open_file)
+        self.chose_button.pack(padx = 5, pady = 10, side = tk.TOP)
+        self.run_button.pack(padx = 5, pady = 10, side = tk.BOTTOM)
+        self.filename = ""
+        self.text = tk.Text(self.master)
+        self.frame.pack()
+        self.text.pack()
+
+    def open_file(self):
+        self.filename = tkFileDialog.askopenfilename()
+        if self.filename :
+            setup_env.display_message(u"選擇檔案: " + self.filename)
+
+    def write(self, massage):
+        self.text.insert(tk.END, massage)
+
+    def run(self):
+        setup_env.display_message(u"開始執行...")
+
+def create_windows():
+    root = tk.Tk()
+    root.title(u"工程認證 B1.0.0.01")
+    root.minsize(width = 600, height = 500)
+    sys.stdout = MainApplication(root)
+    root.mainloop()
+
+create_windows()
