ad2da5169d682fe945827582031b4ed7eb486279
Ruru Chen <balaja2179@gmail.com>
2016-07-10 11:39:27 +0800

    1. Rename the global variable using the upper case. 2. Add function to delete exists folder.
     Changes to be committed:
    	modified:   setup_env.py

--- setup_env.py
+++ setup_env.py
@@ -1,25 +1,43 @@
 #-*- coding: utf-8 -*-
 import os
+import shutil
 import datetime
 
-tmp_folder = ""
+SCRIPT_DIR = os.getcwd()
+TMP_FOLDER = ""
+RESULT_FOLDER = ""
 
-def make_sure_folder_exists(folder):
-    if not os.path.exists(folder):
+def create_folder(folder):
+    try:
         os.makedirs(folder)
         display_message("Create folder: " + folder)
+    except:
+        display_message("Can not create folder : " + folder)
+
+def delete_folder(folder):
+    try:
+        shutil.rmtree(folder)
+        display_message("Delete folder: " + folder)
+    except:
+        display_message("Can not delte folder : " + folder)
+
+def make_sure_folder_exists(folder):
+    # If folder exists, delete folder and create it.
+    if os.path.exists(folder):
+        delete_folder(folder)
+    create_folder(folder)
 
 def get_time():
     return datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 
 def set_environment():
-    global tmp_folder
-    SCRIPT_DIR = os.getcwd()
-    NOW_TIME = get_time()
-    tmp_folder = SCRIPT_DIR + "\\tmp"
-    result_folder = SCRIPT_DIR + "\\" + NOW_TIME
-    make_sure_folder_exists(tmp_folder)
-    make_sure_folder_exists(result_folder)
+    global TMP_FOLDER, SCRIPT_DIR, RESULT_FOLDER
+
+    now_time = get_time()
+    TMP_FOLDER = SCRIPT_DIR + "\\tmp"
+    RESULT_FOLDER = SCRIPT_DIR + "\\" + now_time
+    make_sure_folder_exists(TMP_FOLDER)
+    make_sure_folder_exists(RESULT_FOLDER)
     display_message("Setup Environment successful.")
 
 def display_message(mes):
