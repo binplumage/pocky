87359725c0e14f346ab3a205740dedc91e4b9b9e
Ruru Chen <balaja2179@gmail.com>
2016-07-10 13:38:57 +0800

    1. Rename function from count_student_number to get_split_point. 2. Add function to write entire row data.
     Changes to be committed:
    	modified:   cmp_data.py
    	modified:   rw_data.py

--- cmp_data.py
+++ cmp_data.py
@@ -3,7 +3,7 @@ import setup_env
 import rw_data
 import re
 
-def count_student_number(table, num_rows):
+def get_split_point(table, num_rows):
     STUDENT_ID_COL = 7
     student_id = table.cell(1,STUDENT_ID_COL).value
     # First row is title.
@@ -19,16 +19,17 @@ def split_data(file_name):
     table = rw_data.read_excel(file_name, 0)
     row_num = rw_data.get_row_number(table)
     col_num = rw_data.get_col_number(table)
-    split_point = count_student_number(table, row_num)
+    split_point = get_split_point(table, row_num)
     split_point.append(row_num)
 
     for s in range(len(split_point)-1):
         wb = rw_data.get_init_excel()
         ws = rw_data.get_new_sheet(wb, "sheet 1")
-        for i, j in enumerate(range(split_point[s], split_point[s+1])):
-            sid = table.cell(j, 7).value.rstrip()
-            for k in range(9):
-                ws.write(i, k, table.cell(j, k).value.rstrip())
-        wb.save(setup_env.tmp_folder + "\\" + str(sid) + ".xls")
-        setup_env.display_message(u"分割"+ str(sid) +"...")
-    setup_env.display_message(u"分割結束...")
+        for i, ori_i in enumerate(range(split_point[s], split_point[s+1])):
+            sid = table.cell(ori_i, 7).value.rstrip()
+            if sid[0] == 'B':
+                rw_data.write_all_row_data(ws, table, i, ori_i)
+
+        wb.save(setup_env.TMP_FOLDER + "\\" + str(sid) + ".xls")
+        setup_env.display_message(u"Create "+ str(sid) +".xls tmp file...")
+    setup_env.display_message(u"Split finish...")
--- rw_data.py
+++ rw_data.py
@@ -1,6 +1,8 @@
 import xlrd
 import xlwt
 
+style = xlwt.easyxf('border: top thin, bottom thin, left thin, right thin; font: bold on; align: vert centre, horz center;')
+
 def read_excel(file_name, sheet_number):
     data = xlrd.open_workbook(file_name)
     table = data.sheets()[sheet_number]
@@ -14,7 +16,12 @@ def get_init_excel():
     wb = xlwt.Workbook()
     return wb
 
-def write_data(ws, row, col, value, style):
+def write_all_row_data(ws, table, row, ori_row):
+    for j in range(9):
+        write_data(ws, row, j, table.cell(ori_row, j).value.rstrip())
+
+def write_data(ws, row, col, value):
+    global style
     ws.write(row, col, value, style)
 
 def get_row_number(table):
