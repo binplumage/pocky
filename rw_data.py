a8effbc10583dc31392643a81cf2830ab2106e99
Ruru Chen <balaja2179@gmail.com>
2016-08-14 21:55:27 +0800

    1. Modify the function write_result_data. 2. Get the title column number.
     Changes to be committed:
    	modified:   rw_data.py

--- rw_data.py
+++ rw_data.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
+
 import xlrd
 import xlwt
 import setup_env
 
 default_style = xlwt.easyxf('border: top thin, bottom thin, left thin, right thin; align: vert centre, horz center;')
+
+GET_DATA_TITLE_COL = {}
+
+def get_title_col(table):
+    global GET_DATA_TITLE_COL
+
+    for i, j in enumerate(range(9)):
+        GET_DATA_TITLE_COL[get_cell_value(table,0,j)] = i
 
 def read_excel(file_name, sheet_number):
     try:
@@ -24,6 +33,9 @@ def get_init_excel():
 def copy_all_row_data(ws, table, row, ori_row):
     for i, j in enumerate([ i for i in range(9) if i !=1 ]):
         write_data(ws, row, i, get_cell_value(table, ori_row, j))
+
+def write_merge_data(ws, row, col, value, style=default_style):
+    ws.write_merge(row[0], row[1], col[0], col[1], value, style)
 
 def write_data(ws, row, col, value, style=default_style):
     ws.write(row, col, value, style)
@@ -46,12 +58,12 @@ def get_grade(table, row, register_year):
     return year+semester
 
 def create_title(ws):
-    ws.write_merge(0, 1, 0, 0, u"年級",default_style)
-    ws.write_merge(0, 1, 1, 1, u"課程名稱",default_style)
-    ws.write_merge(0, 1, 2, 2, u"授課教授",default_style)
-    ws.write_merge(0, 1, 3, 3, u"必/選修",default_style)
-    ws.write_merge(0, 0, 4, 7, u"學分數",default_style)
-    ws.write_merge(1, 1, 5, 6, u"工程專業課程\n(含設計實作\n請打V)",default_style)
+    write_merge_data(ws, [0, 1], [0, 0], u"年級")
+    write_merge_data(ws, [0, 1], [1, 1], u"課程名稱")
+    write_merge_data(ws, [0, 1], [2, 2], u"授課教授")
+    write_merge_data(ws, [0, 1], [3, 3], u"必/選修")
+    write_merge_data(ws, [0, 0], [4, 7], u"學分數")
+    write_merge_data(ws, [1, 1], [5, 6], u"工程專業課程\n(含設計實作\n請打V)")
     write_data(ws, 1, 4, u"數學及\n基礎科學",default_style)
     write_data(ws, 1, 7, u"通識課程",default_style)
     ws.row(1).height_mismatch = True
@@ -63,35 +75,50 @@ def write_credit(ws, data, i):
             credit = " "
         write_data(ws, i, col, credit)
 
-def write_processed_data(ws, line_number, data):
+def write_processed_data(ws, row, data):
     for col, value in enumerate(data[0:-1]):
-        write_data(ws, line_number, col, value)
-    write_credit(ws, data[-1], line_number)
+        write_data(ws, row, col, value)
+    write_credit(ws, data[-1], row)
 
 def create_result_title(ws, row):
-    ws.write_merge(row,row,0,3,u"修課總學分數(A)",default_style)
-    ws.write_merge(row+1,row+1,0,3,u"最低畢業學分數(B)",default_style)
-    ws.write_merge(row+1,row+1,4,7,u"136",default_style)
-    ws.write_merge(row+2,row+2,0,3,u"修課佔畢業學分數百分比(A/B)",default_style)
-    ws.write_merge(row+3,row+3,0,3,u"IEET認證規範4課程學分數之要求",default_style)
-    ws.write(row+3,4,u"34學分\n(25%)",default_style)
-    ws.write_merge(row+3,row+3,5,6,u"57學分\n(37.5%)",default_style)
-    ws.write_merge(row+4,row+4,0,3,u"是否符合",default_style)
-    ws.write_merge(row+5,row+5,0,3,u"是否選修實務專題",default_style)
+    write_data(ws, row+3, 4, u"34學分\n(25%)")
+    write_data(ws, row+3,7," ")
+    write_merge_data(ws, [row, row], [0, 3], u"修課總學分數(A)")
+    write_merge_data(ws, [row + 1, row + 1], [0, 3], u"最低畢業學分數(B)")
+    write_merge_data(ws, [row + 1, row + 1], [4, 7], setup_env.GRADUATION_CREDIT_THRESHOLD )
+    write_merge_data(ws, [row + 2, row + 2], [0, 3], u"修課佔畢業學分數百分比(A/B)")
+    write_merge_data(ws, [row + 3, row + 3], [0, 3], u"IEET認證規範4課程學分數之要求")
+    write_merge_data(ws, [row + 4, row + 4], [0, 3], u"是否符合")
+    write_merge_data(ws, [row + 5, row + 5], [0, 3], u"是否選修實務專題")
+    write_merge_data(ws, [row + 3, row + 3], [5, 6], u"57學分\n(37.5%)")
 
-def write_result(ws, line_number, is_project):
+def is_credit_enough(ws, row, col, percent):
+    formula_if = 'IF({0} > {1};"Yes";"No")'.format(str(col[0]) + str(row[0]), percent)
+    transf_to_chinese = (("Yes", u"是") , ("No", u"否"))
+    for i in transf_to_chinese:
+        formula_if = (lambda ori_str, replace_word: ori_str.replace(replace_word[0], replace_word[1]))(formula_if, i)
+    write_data(ws, row[1], col[1], xlwt.Formula(formula_if))
+
+def write_result_data(ws, row, is_project):
     style_percent = xlwt.easyxf(num_format_str='0.00%')
-    col = {4:"E", 5:"F", 7:"H"}
-    for num in [4, 5, 7]:
-        formula_SUM = "SUM("+str(col[num])+"3:"+str(col[num])+str(line_number)+")"
-        ws.write(line_number, num, xlwt.Formula(formula_SUM))
-        formula_AVE = str(col[num])+str(line_number+1)+"/136"
-        ws.write(line_number + 2, num, xlwt.Formula(formula_AVE), style_percent)
-        if num==4:
-            ws.write(line_number + 4, num, xlwt.Formula("IF(%(l)s > 0.25;\"YES\";\"NO\")"% {"l":str(col[num]+str(line_number+3))}))
-        if num==5:
-            ws.write(line_number + 4, num, xlwt.Formula("IF(%(l)s > 0.375;\"YES\";\"NO\")"% {"l":str(col[num]+str(line_number+3))}))
-    if is_project:
-        ws.write(line_number + 5, 4, u"是")
-    else:
-        ws.write(line_number + 5, 4, u"否")
+    col = {4:("E", 0.25), 5:("F", 0.375), 6:("G",None), 7:("H", None)}
+    is_take_project = {True : u"是", False : u"否"}
+
+    for col_num in [4, 5, 7]:
+        formula_SUM = "SUM("+str(col[col_num][0])+"3:"+str(col[col_num][0])+str(row)+")"
+        write_data(ws, row, col_num, xlwt.Formula(formula_SUM))
+
+    for col_num in col:
+        formula_AVE = str(col[col_num][0])+str(row + 1)+"/"+str(setup_env.GRADUATION_CREDIT_THRESHOLD)
+        if col[col_num][1]:
+            is_credit_enough(ws, [row + 3 , row + 4 ], [col[col_num][0], col_num], col[col_num][1])
+            write_data(ws, row + 2, col_num, xlwt.Formula(formula_AVE), style_percent)
+        else:
+            write_data(ws, row + 2, col_num, " ")
+            write_data(ws, row + 4, col_num, " ")
+
+    write_merge_data(ws, [row + 5, row + 5], [4, 7], is_take_project[is_project])
+
+def write_result(ws, row, is_project):
+    create_result_title(ws, row)
+    write_result_data(ws, row, is_project)
